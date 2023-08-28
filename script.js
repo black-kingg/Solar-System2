b123367fac6c2e06129ab1b1103c7009ea16c201
Odetunde Mubarak <mudet04@gmail.com>
2023-08-28 11:26:47 +0100

    #

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+// Declare variables
+let camera, scene, renderer, controls, container, loader;
 
-const controls = new OrbitControls(camera, renderer.domElement);
-const loader = new GLTFLoader();
-
+// Get the container element
 container = document.querySelector(".scene");
+
+// Create the scene
 scene = new THREE.Scene();
 
+// Camera settings
 const fov = 35;
 const aspect = container.clientWidth / container.clientHeight;
 const near = 0.1;
 const far = 500;
-
 camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.set(0, 0, 0);
 
-function init() {}
+// Create the renderer
+renderer = new THREE.WebGL1Renderer({ antialias: true, alpha: true });
+renderer.setSize(container.clientWidth, container.clientHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// Append renderer's DOM element to the container
+container.appendChild(renderer.domElement);
+
+// Load the GLTF model and initialize the scene
+loader = new THREE.GLTFLoader();
+function init() {
+  loader.load("new/scene.gltf", function (gltf) {
+    scene.add(gltf.scene);
+
+    model = gltf.scene;
+    animate(); // Start the animation loop
+  });
+}
+
+// Animation loop
+function animate() {
+  requestAnimationFrame(animate); // Recursive animation loop
+  controls.update(); // Update camera controls
+  renderer.render(scene, camera); // Render the scene
+}
+
+// Initialize the scene
+init();
